67ff46d02b2c2ea8c27c9d8b3c34f5c43a1ddb96
ccphan <phan.cc@icloud.com>
2015-02-02 16:55:55 +0800

    label to display digit pressed

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userInTheMiddleOfTypingANumber: Bool = false
+   
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if userInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            userInTheMiddleOfTypingANumber = true
+            display.text = digit
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
